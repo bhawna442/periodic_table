0910a03e4adcfca121f6c630575a5832cb37e816
Bhawna Golchha <bhawnagolchha03@gmail.com>
2024-09-09 17:01:04 +0000

    fix: the format of output

--- element.sh
+++ element.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
@@ -17,11 +19,11 @@ fi
 #element not in db
 if [[ -z $element ]]
 then
-  echo -e "\nI could not find that element in the database."
+  echo  "I could not find that element in the database."
   exit
 fi
 
 echo $element | while IFS=" |" read an name symbol type mass mp bp 
 do
-  echo -e "\nThe element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+  echo "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
 done
